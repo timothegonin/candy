4bda7bdf7f56020de981d456f7ae6609c11f86ce
timotheG <gonin.timothe@gmail.com>
2024-05-12 15:24:38 +0200

    ✅"addCandies should not alter other inventory"

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -7,7 +7,15 @@ describe('Buy candy', () => {
   })
 });
 describe('Add candy', () => {
+  let currentStore = store.Chamallows.number;
+  currentStore -= 506
+  const otherStore = store.Tagada.number
+
   it('should add candies to inventory', () => {
-    expect(app.addCandies(4, 600)).toEqual(604)
+    expect(app.addCandies(currentStore, 600)).toEqual(604)
+  })
+  it('should not alter the other candies inventory', () => {
+    app.addCandies(currentStore, 600)
+    expect(otherStore).toEqual(510)
   })
 })
