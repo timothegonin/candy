573eaddc87f5d263aab8bd6ba4df15601ea72475
timotheG <gonin.timothe@gmail.com>
2024-05-12 14:45:09 +0200

    🧪 addCandies fail

--- __test__/app.test.js
+++ __test__/app.test.js
@@ -5,4 +5,7 @@ describe('Buy candy', () => {
   it('should remove candies from inventory', () => {
     expect(app.buyCandies(store.Chamallows.number, 20)).toEqual(490)
   })
+  it('should add candies to inventory', () => {
+    expect(app.addCandies(4, 600)).toEqual(604)
+  })
 });
